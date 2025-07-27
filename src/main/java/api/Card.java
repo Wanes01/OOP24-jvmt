bd77268466311e609d186a981645e34de8e3e5d8
Andrea La Tosa <andrea.latosa@studio.unibo.it>
2025-07-27 10:56:13 +0200

    create abstract class Card

---
+++ src/main/java/api/Card.java
@@ -0,0 +1,47 @@
+package api;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public abstract class Card {
+
+    private String name;
+    private TypeCard type;
+    private String imagePath;
+    private BufferedImage imageCard;
+
+    public Card(String name, TypeCard type, String imagePath) {
+        this.name = name;
+        this.type = type;
+        this.imagePath = imagePath;
+
+        try {
+            this.imageCard = ImageIO.read(new File(this.imagePath));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Image not available at path: " + this.imagePath);
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public TypeCard getType() {
+        return this.type;
+    }
+
+    public String getImagePath() {
+        return this.imagePath;
+    }
+
+    public BufferedImage getImageCard() {
+        return this.imageCard;
+    }
+
+    @Override
+    public String toString() {
+        return "Card [getName()=" + getName() + ", getType()=" + getType() + ", getImagePath()=" + getImagePath() + "]";
+    }
+}
