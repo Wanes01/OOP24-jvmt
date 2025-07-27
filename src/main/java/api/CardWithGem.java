30e3d407977afafa0993cb0ad12d84bf27ce4492
Andrea La Tosa <andrea.latosa@studio.unibo.it>
2025-07-27 11:08:36 +0200

    create abstract class CardWithGem

---
+++ src/main/java/api/CardWithGem.java
@@ -0,0 +1,15 @@
+package api;
+
+public abstract class CardWithGem extends Card{
+
+    private int gemValue;
+
+    public CardWithGem(String name, TypeCard type, String imagePath) {
+        super(name, type, imagePath);
+    }
+
+    public int getGemValue() {
+        return this.gemValue;
+    }
+
+}
