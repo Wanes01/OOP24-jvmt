777e8c55c693e7247f2ef2cecd96bbc03bf2e545
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2025-08-08 17:14:24 +0200

    Added implementation for RoundEffect

---
+++ src/main/java/model/impl/round/roundeffect/RoundEffectImpl.java
@@ -0,0 +1,89 @@
+package model.impl.round.roundeffect;
+
+import model.api.round.RoundState;
+import model.api.round.roundeffect.RoundEffect;
+import model.api.round.roundeffect.endcondition.EndCondition;
+import model.api.round.roundeffect.gemmodifier.GemModifier;
+
+/**
+ * Concrete implementation of {@link RoundEffect}.
+ * <p>
+ * This implementation uses {@link EndCondition} to define the condition under
+ * which a round ends and {@link GemModifier} to apply an alteration to the
+ * number of base gems earned by players.
+ * </p>
+ * 
+ * @see RoundEffect
+ * @see GemModifier
+ * @see EndCondition
+ * 
+ * @author Emir Wanes Aouioua
+ */
+public class RoundEffectImpl implements RoundEffect {
+
+    private final EndCondition endCondition;
+    private final GemModifier gemModifier;
+
+    /**
+     * Constructor for the round effect. This implementation delegates control of
+     * the end condition of the round and the calculation of modified gems to the
+     * {@code EndCondition} and {@code GemModifier} objects.
+     * 
+     * @param endCondition the end condition used to determine whether the round has
+     *                     come to an end.
+     * @param gemModifier  the gem modifier used to compute the number of gems
+     *                     earned by players.
+     */
+    public RoundEffectImpl(
+            final EndCondition endCondition,
+            final GemModifier gemModifier) {
+        this.endCondition = endCondition;
+        this.gemModifier = gemModifier;
+    }
+
+    /**
+     * Returns the text description of the effects applied by this RoundEffect. The
+     * resulting description is the union of the {@code EndCondition} and
+     * {@code GemModifier} descriptions with which the object was created.
+     * 
+     * @return the description of the rules this round effect object applys.
+     */
+    @Override
+    public String getDescription() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("* Regole di fine round: ");
+        sb.append(this.endCondition.getDescription());
+        sb.append("* Modificatori di gemme: ");
+        sb.append(this.gemModifier.getDescription());
+        return sb.toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * <p>
+     * Note: this method delegates the decision to the {@code EndCondition} with
+     * which this object is created.
+     * </p>
+     */
+    @Override
+    public boolean isRoundOver(RoundState state) {
+        return this.endCondition.getEndCondition()
+                .test(state);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * <p>
+     * Note: this method delegates the gems transformation to the
+     * {@code GemModifier} with which this object is created.
+     * </p>
+     */
+    @Override
+    public int applyGemModifier(RoundState state, int gems) {
+        return this.gemModifier.getGemModifier()
+                .apply(state, gems);
+    }
+
+}
