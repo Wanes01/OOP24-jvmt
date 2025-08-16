28bbe3bffbc1618d40467cf1441cb8927b7a0b77
Andrea La Tosa <andrea.latosa@studio.unibo.it>
2025-08-16 22:51:37 +0200

    Removed internal management of the card image and adapted the tests

--- src/test/java/card/RelicCardTest.java
+++ src/test/java/card/RelicCardTest.java
@@ -12,8 +12,10 @@ import model.card.impl.TrapCard;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -26,17 +28,24 @@ class RelicCardTest {
     @BeforeEach
     void setUp() { }
 
+    // Check that if the card name is null, it throws an exception
+    @Test
+    void inputCardNameNull() {
+        assertThrows(NullPointerException.class, () -> new RelicCard(null));
+    }
+
     // Verification of getter methods for relic cards
     @Test
     void correctGetterRelicCard() {
         final RelicCard relicCard = new RelicCard("Relic");
         final String imagePathRelic = "/imageCard/relic/Relic.png";
+        final URL imageUrlRelic = Card.class.getResource(imagePathRelic);
         final List<Integer> possibleValues = List.of(5, 7, 8, 10, 12);
 
         assertEquals("Relic", relicCard.getName());
         assertTrue(possibleValues.contains(relicCard.getGemValue()));
         assertEquals(TypeCard.RELIC, relicCard.getType());
-        assertEquals(imagePathRelic, relicCard.getImagePath());
+        assertEquals(imageUrlRelic, relicCard.getImagePath());
         assertFalse(relicCard.isRedeemed());
         relicCard.redeemCard();
         assertTrue(relicCard.isRedeemed());
