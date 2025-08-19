0b05a5b157fc8ce66e5c3bcdecb227d97fee3a94
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2025-08-19 19:46:12 +0200

    Added Swing implementations for Window and Page interfaces

---
+++ src/main/java/view/page/api/SwingPage.java
@@ -0,0 +1,112 @@
+package view.page.api;
+
+import javax.swing.JPanel;
+
+import controller.PageController;
+
+/**
+ * Abstract class for {@link Page}s that use the Swing library.
+ * <p>
+ * A {@code SwingPage} extends {@link JPanel} and implements the {@link Page}
+ * interface. This class is designed to be extended by concrete pages of the
+ * application.
+ * </p>
+ * <p>
+ * Each page is associated with a {@link PageController}, which is set via
+ * {@link #setController(PageController)}. Subclasses must implement
+ * {@link #setHandlers()} to configure event listeners, binding GUI interactions
+ * to the controller.
+ * </p>
+ * <p>
+ * Note: the subclasses can retrieve the specific controller for the page
+ * they represent by using the {@link #getController(ControllerClass)} method.
+ * This design choice was made to avoid propagating the use of generics to
+ * pages.
+ * </p>
+ * 
+ * @see SwingWindow
+ * @see Page
+ * @see JPanel
+ * 
+ * @author Emir Wanes Aouioua
+ */
+public abstract class SwingPage extends JPanel implements Page {
+
+    private PageController controller;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void display() {
+        this.setVisible(true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void dismiss() {
+        this.setVisible(false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void refresh() {
+        this.revalidate();
+        this.repaint();
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * <p>
+     * Initilizes event binding as soon as the controller is set.
+     * </p>
+     * <p>
+     * <strong>Note:</strong>
+     * the specific controller for a concrete page can
+     * be retrieved using {@link #getController(ControllerClass)}.
+     * </p>
+     */
+    @Override
+    public void setController(PageController controller) {
+        this.controller = controller;
+        this.setHandlers();
+    }
+
+    /**
+     * Configures the event handlers that binds the GUI components
+     * of this page with the controller logic.
+     * <p>
+     * Sublasses <strong>must</strong> implement this method to define how user
+     * interactions are handled.
+     * </p>
+     */
+    protected abstract void setHandlers();
+
+    /**
+     * Returns the controller associated with this page casted to a specific type.
+     * <p>
+     * This method ensures that the access to the controller is safe with a runtime
+     * check.
+     * This method was created to avoid propagating the use of generics to pages.
+     * </p>
+     * 
+     * @param <T>             the expected type of the controller, namely a
+     *                        controller that extends {@link PageController}.
+     * @param controllerClass the class object of the expected controller.
+     * @return the controller casted to the specified type.
+     * @throws IllegalArgumentException if the controller class is not an extension
+     *                                  of {@link PageController}.
+     */
+    protected <T extends PageController> T getController(Class<T> controllerClass) {
+        if (controllerClass.isInstance(this.controller)) {
+            return controllerClass.cast(this.controller);
+        }
+        throw new IllegalArgumentException("The controller must extend PageContoller");
+    }
+
+}
---
+++ src/main/java/view/window/impl/SwingWindow.java
@@ -0,0 +1,145 @@
+package view.window.api;
+
+import java.util.Optional;
+
+import javax.swing.JFrame;
+
+import view.page.api.Page;
+import view.page.api.SwingPage;
+
+/**
+ * Swing based implementation of the {@link Window} interface.
+ * <p>
+ * {@code SwingWindow} extends {@link JFrame} and serves as a container
+ * for {@link SwingPage}s. This implementation manages a single
+ * {@link SwingPage} at a time as its current content.
+ * </p>
+ * <p>
+ * The window can not be displayed or refreshed unless a page
+ * has been set via {@link #setCurrentPage(Page)}, an
+ * {@link IllegalStateException} will be thrown otherwise.
+ * </p>
+ * <p>
+ * By default, the window is created with a size of {@code 1280x720} pixels,
+ * unless a custom size is provided.
+ * </p>
+ * 
+ * @see Window
+ * @see SwingPage
+ * @see Page
+ * 
+ * @author Emir Wanes Aouioua
+ */
+public class SwingWindow extends JFrame implements Window {
+
+    private static final int DEFAULT_WIDTH = 1280;
+    private static final int DEFAULT_HEIGHT = 720;
+    private Optional<SwingPage> currentPage = Optional.empty();
+
+    /**
+     * Creates a new swing window with the given dimensions.
+     * 
+     * @param width  the window's width, in pixels.
+     * @param height the window's height, in pixels.
+     */
+    public SwingWindow(int width, int height) {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        super.setSize(width, height);
+    }
+
+    /**
+     * Creates a new swing window with a default size of {@code 1280x720} pixels.
+     */
+    public SwingWindow() {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws IllegalStateException if a {@link SwingPage} is not set.
+     */
+    @Override
+    public void display() {
+        this.checkEmptyAndThrowStateException("Can't display an empty window. Set the current page first.");
+        this.setVisible(true);
+    }
+
+    /**
+     * Utility method that checks if the current {@link SwingPage} is set.
+     * 
+     * @param message the message to use in the exception if a page is not set.
+     * @throws IllegalStateException if a {@link SwingPage} is not set.
+     */
+    private void checkEmptyAndThrowStateException(String message) {
+        if (this.currentPage.isEmpty()) {
+            throw new IllegalStateException(message);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void dismiss() {
+        this.setVisible(false);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws IllegalStateException if a {@link SwingPage} is not set.
+     */
+    @Override
+    public void refresh() {
+        this.checkEmptyAndThrowStateException("Can't refresh an empty window. Set the current page first.");
+        this.revalidate(); // computes the new components position and sizes.
+        this.repaint();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void disableInteraction() {
+        this.setEnabled(false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void enableInteraction() {
+        this.setEnabled(true);
+    }
+
+    /**
+     * Terminates the whole application.
+     */
+    @Override
+    public void close() {
+        this.dispose();
+        System.exit(0);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Only {@link SwingPage} instances are supported.
+     * </p>
+     * 
+     * @param page the SwingPage to display.
+     * @throws IllegalArgumentException if the page is not a {@link SwingPage}.
+     */
+    @Override
+    public void setCurrentPage(Page page) {
+        if (!(page instanceof SwingPage)) {
+            throw new IllegalArgumentException("View non compatibile con Swing");
+        }
+        final SwingPage swingPage = (SwingPage) page;
+        this.currentPage = Optional.of(swingPage);
+        this.setContentPane(swingPage);
+        this.refresh();
+    }
+
+}
