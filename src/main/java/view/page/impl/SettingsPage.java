00654e5d16ee31fed332310e870dd74688ebe2bc
Andrea La Tosa <andrea.latosa@studio.unibo.it>
2025-08-26 19:59:36 +0200

    Refactored SettingPage to use JSpinnerWithLabel

--- src/main/java/view/page/impl/SettingsPage.java
+++ src/main/java/view/page/impl/SettingsPage.java
@@ -1,22 +1,24 @@
 package view.page.impl;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
 import javax.swing.JTextArea;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 
+import controller.api.PageController;
+import controller.impl.SettingsControllerImpl;
 import model.card.api.TypeDeck;
 import model.player.api.CpuDifficulty;
 import model.round.api.roundeffect.endcondition.EndCondition;
@@ -25,8 +27,10 @@ import model.round.api.roundeffect.gemmodifier.GemModifier;
 import model.round.api.roundeffect.gemmodifier.GemModifierFactory;
 import model.round.impl.roundeffect.endcondition.EndConditionFactoryImpl;
 import model.round.impl.roundeffect.gemmodifier.GemModifierFactoryImpl;
+import model.settings.impl.GameSettingsImpl;
 import view.page.api.SwingPage;
 import view.page.utility.ComboboxWithLabel;
+import view.page.utility.JSpinnerWithLabel;
 
 /**
  * Displays the game settings page.
@@ -47,234 +51,231 @@ import view.page.utility.ComboboxWithLabel;
  */
 public class SettingsPage extends SwingPage {
 
-        private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-        private static final List<GemModifier> GEM_MODIFIERS = createGemModifiers();
-        private static final List<EndCondition> END_CONDITIONS = createEndConditions();
+    private static final List<GemModifier> GEM_MODIFIERS = createGemModifiers();
+    private static final List<EndCondition> END_CONDITIONS = createEndConditions();
 
-        private static final Dimension RELATED_SPACE = new Dimension(0, 10);
-        private static final Dimension UNRELATED_SPACE = new Dimension(0, 50);
-        private static final Dimension SPACE_BETWEEN_COL = new Dimension(100, 0);
+    private static final Dimension RELATED_SPACE = new Dimension(0, 10);
+    private static final Dimension UNRELATED_SPACE = new Dimension(0, 50);
+    private static final Dimension SPACE_BETWEEN_COL = new Dimension(50, 0);
 
-        // Behaviors common to all JSpinners
-        private static final int SPN_STEP_SIZE = 1;
-        private static final Dimension SPN_DIMENSION = new Dimension(200, 40);
+    // Behaviors common to all JSpinners
+    private static final int SPN_STEP_SIZE = 1;
+    private static final Dimension SPN_DIMENSION = new Dimension(200, 40);
 
-        /**
-         * Create the panel to be displayed by adding 3 columns.
-         */
-        public SettingsPage() {
-                final JPanel settingsPanel = new JPanel();
-                settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.X_AXIS));
 
-                settingsPanel.add(settingsCol1());
-                settingsPanel.add(Box.createRigidArea(SPACE_BETWEEN_COL));
-                settingsPanel.add(settingsCol2());
-                settingsPanel.add(Box.createRigidArea(SPACE_BETWEEN_COL));
-                settingsPanel.add(settingsCol3());
+    private JTextArea txtAreaName;
+    private JButton btnPlay;
 
-                super.add(settingsPanel);
-        }
+    /**
+     * Create the panel to be displayed by adding 3 columns.
+     */
+    public SettingsPage() {	
+        final JPanel mainPanel = new JPanel(new BorderLayout(100, 100));
 
-        // Create the list with the end of round conditions
-        private static List<EndCondition> createEndConditions() {
-                final EndConditionFactory endCond = new EndConditionFactoryImpl();
-                return List.of(
-                                endCond.standard(),
-                                endCond.firstTrapEnds());
-        }
+        final JPanel settingsPanel = new JPanel();
+        settingsPanel.setLayout(new BoxLayout(settingsPanel, BoxLayout.X_AXIS));
 
-        // Create the list of gem modifiers to apply to the rounds
-        private static List<GemModifier> createGemModifiers() {
-                final GemModifierFactory gemFact = new GemModifierFactoryImpl();
-                return List.of(
-                                gemFact.standard(),
-                                gemFact.gemMultiplier(2),
-                                gemFact.gemMultiplier(3),
-                                gemFact.riskyReward(10),
-                                gemFact.leftReward(ABORT));
-        }
+        mainPanel.add(settingsPanel);
+        final int margin = 15;
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(
+        margin, margin, margin, margin));
 
-        /**
-         * @return a panel containing the first column to be displayed.
-         *         <p>
-         *         This contains:
-         *         </p>
-         *         <ul>
-         *         <li>player names</li>
-         *         <li>CPU number</li>
-         *         <li>CPU difficulty</li>
-         *         </ul>
-         */
-        private JPanel settingsCol1() {
+        settingsPanel.add(settingsCol1());
+        settingsPanel.add(Box.createRigidArea(SPACE_BETWEEN_COL));
+        settingsPanel.add(settingsCol2());
+        settingsPanel.add(Box.createRigidArea(SPACE_BETWEEN_COL));
+        settingsPanel.add(settingsCol3());
 
-                final JPanel settingsCol1;
-                final JLabel lblDescrName;
-                final JTextArea txtAreaName;
-                final JScrollPane scrlPaneName;
-                final JLabel lblNumPlayer;
-                final SpinnerNumberModel modelSpinner;
-                final JSpinner jspNumCPU;
-                final ComboboxWithLabel<CpuDifficulty> difficulty;
+        super.add(mainPanel);
+    }
 
-                final int jtxtareaWidth = 10;
-                final int jtxtareaHeight = 30;
+    // Create the list with the end of round conditions
+    private static List<EndCondition> createEndConditions() {
+        final EndConditionFactory endCond = new EndConditionFactoryImpl();
+        return List.of(
+            endCond.standard(),
+            endCond.firstTrapEnds());
+    }
 
-                final int spnStartValue = 0;
-                final int spnMinValue = 0;
-                final int spnMaxValue = 8;
+    // Create the list of gem modifiers to apply to the rounds
+    private static List<GemModifier> createGemModifiers() {
+        final GemModifierFactory gemFact = new GemModifierFactoryImpl();
+        return List.of(
+            gemFact.standard(),
+            gemFact.gemMultiplier(2),
+            gemFact.gemMultiplier(3),
+            gemFact.riskyReward(10),
+            gemFact.leftReward(ABORT));
+    }
 
-                settingsCol1 = new JPanel();
-                settingsCol1.setLayout(new BoxLayout(settingsCol1, BoxLayout.Y_AXIS));
+    /**
+     * @return a panel containing the first column to be displayed.
+     *         <p>
+     *         This contains:
+     *         </p>
+     *         <ul>
+     *         <li>player names</li>
+     *         <li>CPU number</li>
+     *         <li>CPU difficulty</li>
+     *         </ul>
+     */
+    private JPanel settingsCol1() {
+        final JPanel settingsCol1;
+        final JLabel lblDescrName;
+        final JScrollPane scrlPaneName;
+        final JSpinnerWithLabel numCPU;
+        final ComboboxWithLabel<CpuDifficulty> difficulty;
 
-                lblDescrName = new JLabel(
-                                "<html><div style='text-align: center;'>"
-                                                + "Inserire i nomi dei giocatori<br>(uno per riga):</div></html>");
-                lblDescrName.setHorizontalAlignment(SwingConstants.CENTER);
-                lblDescrName.setAlignmentX(CENTER_ALIGNMENT);
+        final int jtxtareaWidth = 10;
+        final int jtxtareaHeight = 30;
 
-                txtAreaName = new JTextArea(jtxtareaWidth, jtxtareaHeight);
-                txtAreaName.setWrapStyleWord(true);
-                scrlPaneName = new JScrollPane(txtAreaName);
-                scrlPaneName.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-                scrlPaneName.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        final int spnStartValue = 0;
+        final int spnMinValue = 0;
+        final int spnMaxValue = GameSettingsImpl.MAX_ROUNDS;
 
-                lblNumPlayer = new JLabel("Inserire il numero di CPU:");
-                lblNumPlayer.setAlignmentX(CENTER_ALIGNMENT);
+        settingsCol1 = new JPanel();
+        settingsCol1.setLayout(new BoxLayout(settingsCol1, BoxLayout.Y_AXIS));
 
-                modelSpinner = new SpinnerNumberModel(spnStartValue, spnMinValue, spnMaxValue, SPN_STEP_SIZE);
-                jspNumCPU = new JSpinner(modelSpinner);
-                jspNumCPU.setPreferredSize(SPN_DIMENSION);
-                jspNumCPU.setMaximumSize(SPN_DIMENSION);
-                jspNumCPU.setAlignmentX(CENTER_ALIGNMENT);
+        lblDescrName = new JLabel(
+            "<html><div style='text-align: center;'>"
+                + "Enter the names of the players<br> (one per line):</div></html>");
+        lblDescrName.setHorizontalAlignment(SwingConstants.CENTER);
+        lblDescrName.setAlignmentX(CENTER_ALIGNMENT);
 
-                difficulty = new ComboboxWithLabel<>(
-                                "Inserire difficoltà CPU:",
-                                Arrays.asList(CpuDifficulty.values()),
-                                RELATED_SPACE);
+        this.txtAreaName = new JTextArea(jtxtareaWidth, jtxtareaHeight);
+        this.txtAreaName.setWrapStyleWord(true);
+        scrlPaneName = new JScrollPane(txtAreaName);
+        scrlPaneName.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrlPaneName.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-                settingsCol1.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol1.add(lblDescrName);
-                settingsCol1.add(Box.createRigidArea(RELATED_SPACE));
-                settingsCol1.add(scrlPaneName);
-                settingsCol1.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol1.add(lblNumPlayer);
-                settingsCol1.add(Box.createRigidArea(RELATED_SPACE));
-                settingsCol1.add(jspNumCPU);
-                settingsCol1.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol1.add(difficulty.getPanel());
+        numCPU = new JSpinnerWithLabel(
+            "Enter the number of CPU players:",
+            spnStartValue,
+            spnMinValue, 
+            spnMaxValue, 
+            SPN_STEP_SIZE, 
+            SPN_DIMENSION, 
+            RELATED_SPACE);
 
-                return settingsCol1;
-        }
+        difficulty = new ComboboxWithLabel<>(
+            "Enter CPU difficulty:",
+            Arrays.asList(CpuDifficulty.values()),
+            RELATED_SPACE);
 
-        /**
-         * @return a panel containing the second column to be displayed.
-         *         <p>
-         *         This contains:
-         *         </p>
-         *         <ul>
-         *         <li>Deck type</li>
-         *         <li>Round end condition</li>
-         *         <li>Round gem modifier</li>
-         *         <li>Total number of rounds</li>
-         *         </ul>
-         */
-        private JPanel settingsCol2() {
-                final JPanel settingsCol2;
-                final ComboboxWithLabel<TypeDeck> deckType;
-                final JLabel lblNumRound;
-                final SpinnerNumberModel modelSpinner;
-                final JSpinner jspNumRound;
-                final ComboboxWithLabel<String> gemMod;
-                final ComboboxWithLabel<String> endCond;
+        settingsCol1.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol1.add(lblDescrName);
+        settingsCol1.add(Box.createRigidArea(RELATED_SPACE));
+        settingsCol1.add(scrlPaneName);
+        settingsCol1.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol1.add(numCPU.getPanel());
+        settingsCol1.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol1.add(difficulty.getPanel());
 
-                final int spnStartValue = 1;
-                final int spnMinValue = 1;
-                final int spnMaxValue = 20;
-                final List<String> gemModifiersDescr;
-                final List<String> endCondDescr;
+        return settingsCol1;
+    }
 
-                settingsCol2 = new JPanel();
-                settingsCol2.setLayout(new BoxLayout(settingsCol2, BoxLayout.Y_AXIS));
+    /**
+     * @return a panel containing the second column to be displayed.
+     *         <p>
+     *         This contains:
+     *         </p>
+     *         <ul>
+     *         <li>Deck type</li>
+     *         <li>Round end condition</li>
+     *         <li>Round gem modifier</li>
+     *         <li>Total number of rounds</li>
+     *         </ul>
+     */
+    private JPanel settingsCol2() {
+        final JPanel settingsCol2;
+        final ComboboxWithLabel<TypeDeck> deckType;
+        final ComboboxWithLabel<String> gemMod;
+        final ComboboxWithLabel<String> endCond;
+        final JSpinnerWithLabel numRound;
 
-                deckType = new ComboboxWithLabel<>(
-                                "Selezionare il tipo di mazzo:",
-                                Arrays.asList(TypeDeck.values()),
-                                RELATED_SPACE);
+        final int spnStartValue = 1;
+        final int spnMinValue = 1;
+        final int spnMaxValue = GameSettingsImpl.MAX_ROUNDS;
+        final List<String> gemModifiersDescr;
+        final List<String> endCondDescr;
 
-                lblNumRound = new JLabel("Inserire il numero di round:");
-                lblNumRound.setHorizontalAlignment(SwingConstants.CENTER);
-                lblNumRound.setAlignmentX(CENTER_ALIGNMENT);
-                modelSpinner = new SpinnerNumberModel(spnStartValue, spnMinValue, spnMaxValue, SPN_STEP_SIZE);
-                jspNumRound = new JSpinner(modelSpinner);
-                jspNumRound.setPreferredSize(SPN_DIMENSION);
-                jspNumRound.setMaximumSize(SPN_DIMENSION);
-                jspNumRound.setAlignmentX(CENTER_ALIGNMENT);
+        settingsCol2 = new JPanel();
+        settingsCol2.setLayout(new BoxLayout(settingsCol2, BoxLayout.Y_AXIS));
 
-                gemModifiersDescr = GEM_MODIFIERS.stream()
-                                .map(GemModifier::getDescription)
-                                .collect(Collectors.toList());
+        deckType = new ComboboxWithLabel<>(
+            "Select the type of deck:",
+            Arrays.asList(TypeDeck.values()),
+            RELATED_SPACE);
 
-                endCondDescr = END_CONDITIONS.stream()
-                                .map(EndCondition::getDescription)
-                                .collect(Collectors.toList());
+        endCondDescr = END_CONDITIONS.stream()
+            .map(EndCondition::getDescription)
+            .collect(Collectors.toList());
 
-                endCond = new ComboboxWithLabel<>(
-                                "<html><div style='text-align: center;'>"
-                                                + "Selezionare la condizione di fine<br>dei round:</div></html>",
-                                endCondDescr,
-                                RELATED_SPACE);
+        endCond = new ComboboxWithLabel<>(
+            "<html><div style='text-align: center;'>"
+                            + "Select the end condition<br>for the rounds:</div></html>",
+            endCondDescr,
+            RELATED_SPACE);
 
-                gemMod = new ComboboxWithLabel<>(
-                                "<html><div style='text-align: center;'>"
-                                                + "Selezionare il modificatore<br>di gemme dei round:</div></html>",
-                                gemModifiersDescr,
-                                RELATED_SPACE);
+        gemModifiersDescr = GEM_MODIFIERS.stream()
+            .map(GemModifier::getDescription)
+            .collect(Collectors.toList());
 
-                settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol2.add(deckType.getPanel());
-                settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol2.add(endCond.getPanel());
-                settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol2.add(gemMod.getPanel());
-                settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
-                settingsCol2.add(lblNumRound);
-                settingsCol2.add(Box.createRigidArea(RELATED_SPACE));
-                settingsCol2.add(jspNumRound);
+        gemMod = new ComboboxWithLabel<>(
+            "<html><div style='text-align: center;'>"
+                            + "Select the gem modifier<br>for the rounds:</div></html>",
+            gemModifiersDescr,
+            RELATED_SPACE);
 
-                return settingsCol2;
-        }
+        numRound = new JSpinnerWithLabel(
+            "Enter the number of rounds:", 
+            spnStartValue, 
+            spnMinValue, 
+            spnMaxValue, 
+            SPN_STEP_SIZE, 
+            SPN_DIMENSION, 
+            RELATED_SPACE);
 
-        /**
-         * @return a panel containing the third column to be displayed.
-         *         It contains the button to start the game.
-         * 
-         */
-        private JPanel settingsCol3() {
-                final JPanel settingsCol3;
-                final JButton btnPlay;
+        settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol2.add(deckType.getPanel());
+        settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol2.add(endCond.getPanel());
+        settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol2.add(gemMod.getPanel());
+        settingsCol2.add(Box.createRigidArea(UNRELATED_SPACE));
+        settingsCol2.add(numRound.getPanel());
 
-                final Font btnFont = new Font("Arial", Font.BOLD, 50);
+        return settingsCol2;
+    }
 
-                settingsCol3 = new JPanel();
-                settingsCol3.setLayout(new BoxLayout(settingsCol3, BoxLayout.Y_AXIS));
+    /**
+     * @return a panel containing the third column to be displayed.
+     *         It contains the button to start the game.
+     * 
+     */
+    private JPanel settingsCol3() {
+            final JPanel settingsCol3;
 
-                btnPlay = new JButton("Avvia Partita");
-                btnPlay.setFont(btnFont);
-                btnPlay.setAlignmentX(CENTER_ALIGNMENT);
+            final Font btnFont = new Font("Arial", Font.BOLD, 50);
 
-                settingsCol3.add(btnPlay);
+            settingsCol3 = new JPanel();
+            settingsCol3.setLayout(new BoxLayout(settingsCol3, BoxLayout.Y_AXIS));
 
-                return settingsCol3;
-        }
+            btnPlay = new JButton("Avvia Partita");
+            btnPlay.setFont(btnFont);
+            btnPlay.setAlignmentX(CENTER_ALIGNMENT);
 
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        protected void setHandlers() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'setHandlers'");
-        }
+            settingsCol3.add(btnPlay);
 
+            return settingsCol3;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void setHandlers() {
+    }
 }
