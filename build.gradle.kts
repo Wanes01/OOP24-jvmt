6cc79bf4731940a69d339bda2891b79661ae1ab7
Filippo Gaggi <filippo.gaggi@studio.unibo.it>
2025-08-27 02:14:43 +0200

    Added dependencies for Mockito in the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,11 @@ dependencies {
     // when dependencies share the same version, grouping in a val helps to keep them in sync
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+
+    //For testing
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.8.0")
 }
 
 /* application {
