a78ad5fdb926730a2fd65e8229043e864f957d06
Filippo Gaggi <filippo.gaggi@studio.unibo.it>
2025-09-07 12:06:03 +0200

    Corrected PlayerChoice javadoc

--- src/main/java/model/player/api/PlayerChoice.java
+++ src/main/java/model/player/api/PlayerChoice.java
@@ -1,10 +1,15 @@
 package model.player.api;
 
+import model.player.impl.PlayerInRound;
+
 /**
  * Enum containing the player's choices that
  * can be made at the end of the turn.
  * 
  * @see PlayerInRound
+ * 
+ * @param STAY  choice that represents if a player is staying.
+ * @param EXIT  choice that represents if a player is exited.
  * 
  * @author Filippo Gaggi
  */
