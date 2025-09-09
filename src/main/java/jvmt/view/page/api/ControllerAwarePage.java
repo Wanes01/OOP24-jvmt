06d8e2a1b1b3fdbfa25ff2814a8d1ac0fbf463fc
Emir Wanes Aouioua <emirwanes.aouioua@studio.unibo.it>
2025-09-09 17:06:15 +0200

    Fixed javadoc

--- src/main/java/jvmt/view/page/api/ControllerAwarePage.java
+++ src/main/java/jvmt/view/page/api/ControllerAwarePage.java
@@ -1,5 +1,7 @@
 package jvmt.view.page.api;
 
+import java.util.Optional;
+
 import jvmt.controller.api.PageController;
 
 /**
@@ -7,14 +9,34 @@ import jvmt.controller.api.PageController;
  * a {@link PageController}.
  * <p>
  * This class provides a way to bind a controller to a page
- * and to retrieve it a type-safe way.
+ * and to retrieve it in a type-safe way, without propagating the use
+ * of generics into implementations.
+ * </p>
+ * <p>
+ * Each page is associated with a {@link PageController}, which is set via
+ * {@link #setController(PageController)}. Subclasses must implement
+ * {@link #setHandlers()} to configure event listeners, binding GUI interactions
+ * to the controller.
+ * </p>
+ * <p>
+ * Note: the subclasses can retrieve the specific controller for the page
+ * they represent by using the {@link #getController(ControllerClass)} method.
+ * This design choice was made to avoid propagating the use of generics to
+ * pages.
  * </p>
  */
 public abstract class ControllerAwarePage implements Page {
     /**
      * the controller that will handle the interaction with this page.
      */
-    private PageController controller;
+    private Optional<PageController> controller;
+
+    /**
+     * Initializes this page with no controller bound to it.
+     */
+    protected ControllerAwarePage() {
+        this.controller = Optional.empty();
+    }
 
     /**
      * {@inheritDoc}
@@ -29,8 +51,8 @@ public abstract class ControllerAwarePage implements Page {
      * </p>
      */
     @Override
-    public void setController(PageController controller) {
-        this.controller = controller;
+    public void setController(final PageController controller) {
+        this.controller = Optional.of(controller);
         this.setHandlers();
     }
 
@@ -58,10 +80,14 @@ public abstract class ControllerAwarePage implements Page {
      * @return the controller casted to the specified type.
      * @throws IllegalArgumentException if the controller class is not an extension
      *                                  of {@link PageController}.
+     * @throws IllegalStateException    if the controller has not been set yet.
      */
-    protected <T extends PageController> T getController(Class<T> controllerClass) {
-        if (controllerClass.isInstance(this.controller)) {
-            return controllerClass.cast(this.controller);
+    protected <T extends PageController> T getController(final Class<T> controllerClass) {
+        if (this.controller.isEmpty()) {
+            throw new IllegalStateException("A controller has not been set yet.");
+        }
+        if (controllerClass.isInstance(this.controller.get())) {
+            return controllerClass.cast(this.controller.get());
         }
         throw new IllegalArgumentException("The controller must extend PageController");
     }
--- src/main/java/jvmt/view/page/api/SwingPage.java
+++ src/main/java/jvmt/view/page/api/SwingPage.java
@@ -1,27 +1,20 @@
 package jvmt.view.page.api;
 
 import javax.swing.JPanel;
-import jvmt.controller.api.PageController;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jvmt.view.window.impl.SwingWindow;
 
 /**
  * Abstract class for {@link Page}s that use the Swing library.
  * <p>
- * A {@code SwingPage} extends {@link JPanel} and implements the {@link Page}
+ * A {@code SwingPage} extends {@link ControllerAwarePage} and implements the
+ * {@link Page}
  * interface. This class is designed to be extended by concrete pages of the
  * application.
  * </p>
  * <p>
- * Each page is associated with a {@link PageController}, which is set via
- * {@link #setController(PageController)}. Subclasses must implement
- * {@link #setHandlers()} to configure event listeners, binding GUI interactions
- * to the controller.
- * </p>
- * <p>
- * Note: the subclasses can retrieve the specific controller for the page
- * they represent by using the {@link #getController(ControllerClass)} method.
- * This design choice was made to avoid propagating the use of generics to
- * pages.
+ * This implementation wraps a {@link JPanel} to set the visual components.
  * </p>
  * 
  * @see SwingWindow
@@ -72,6 +65,8 @@ public abstract class SwingPage extends ControllerAwarePage {
      * 
      * @return the {@code JPanel} used by this page implementation.
      */
+    @SuppressFBWarnings(value = { "EI_EXPOSE_REP",
+            "EI_EXPOSE_REP2" }, justification = "The shared panel is part of the view design")
     public JPanel getPanel() {
         return this.panel;
     }
