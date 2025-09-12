9b8e64fcafdd18469d365c20f4e89b177838d7fd
Filippo Gaggi <filippo.gaggi@studio.unibo.it>
2025-09-12 12:33:17 +0200

    Added informations in CpuDifficulty javadoc

--- src/main/java/jvmt/model/player/api/CpuDifficulty.java
+++ src/main/java/jvmt/model/player/api/CpuDifficulty.java
@@ -8,10 +8,21 @@ package jvmt.model.player.api;
  * @author Filippo Gaggi
  */
 public enum CpuDifficulty {
-    /** Represents the easy difficulty. */
+    /**
+     * Represents the easy difficulty.
+     * In this difficulty gems on the path weight the most,
+     * traps and relics weight the less.
+     */
     EASY,
-    /** Represents the normal difficulty. */
+    /**
+     * Represents the normal difficulty.
+     * In this difficulty all the weights are balanced 
+     */
     NORMAL,
-    /** Represents the hard difficulty. */
+    /**
+     * Represents the hard difficulty.
+     * In this difficulty traps and relics weight the most,
+     * gems on the path and active players weight the less.
+     */
     HARD
 }
